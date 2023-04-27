ad6586dafa3bcdf302522a227eb73dd261a91988
Julio Ellies <julioelliesflores@gmail.com>
2023-04-26 21:59:33 -0400

    clase 26-04

---
+++ ajax/index.js
@@ -0,0 +1,28 @@
+function buscarPokemon(){
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then(respuesta => respuesta.json())
+        .then(data => {
+            // console.log(data)
+            
+            let nombre_pokemon = data.name;
+            let habilidades = data.abilities;
+            document.getElementById('main').innerHTML += nombre_pokemon + "<br>";
+
+            for (let i = 0; i < habilidades.length; i++) {
+
+                // console.log(habilidades[i]);
+                console.log(habilidades[i].ability.name);
+                let habilidad = habilidades[i].ability.name;
+                document.getElementById('main').innerHTML += `habilidad : ${habilidad} <br>`;
+                
+            }
+
+
+
+
+        })
+        .catch(err => console.error(err))
+}
+
+buscarPokemon();
+
